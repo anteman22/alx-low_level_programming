6bf7a26deddfeb5ea235529a29e7b5e772bee492
anteman22 <anteman22@gmailcom>
2022-04-08 13:51:54 -0700

    low level

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 /**
  *  * main - main block
- *   * Description: Get a random number and check its last digit, compare it with 5
- *    * Return: 0
+ *   * Return: 0
  */
 int main(void)
 {
