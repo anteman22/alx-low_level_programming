23cb6281576246d371fe7e99cd1264a008461352
anteman22 <anteman22@gmailcom>
2022-04-12 02:10:49 -0700

    assignment on functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ *  main - check the code
+
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+void print_alphabet(void)
+{
+	char alp = 'a';
+	while (alp <= 'z')
+{
+	_putchar (alp);
+	alp++;
+}
+	_putchar ('\n');
+
+}
+}
+
